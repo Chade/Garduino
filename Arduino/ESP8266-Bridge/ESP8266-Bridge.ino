95399dee89b583794e2ed100dfc62f169773589b
Chade <Chade@users.noreply.github.com>
2019-07-05 21:27:01 +0200

    Using EspAsyncWebServer

--- Arduino/ESP8266-Bridge/ESP8266-Bridge.ino
+++ Arduino/ESP8266-Bridge/ESP8266-Bridge.ino
@@ -11,7 +11,6 @@ struct Channel {
 void parseToXml(Stream& stream, const Channel* ch, const byte& numCh) {
   stream.println("<?xml version = \"1.0\" ?>");
   stream.println("<channels>");
-
   for (byte i = 0; i < numCh; i++) {
     stream.println("<channel id=\"" + String(i) + "\">");
     stream.println("<enabled>"  + ch[i].enabled   + "</enabled>");
@@ -21,6 +20,20 @@ void parseToXml(Stream& stream, const Channel* ch, const byte& numCh) {
     stream.println("<repeat>"   + ch[i].repeat    + "</repeat>");
     stream.println("</channel>");
   }
+  stream.println("</channels>");
+  stream.println('\n');
+}
+
+void parseToXml(Stream& stream, const Channel& ch, const byte& idx) {
+  stream.println("<?xml version = \"1.0\" ?>");
+  stream.println("<channels>");
+  stream.println("<channel id=\"" + String(idx) + "\">");
+  stream.println("<enabled>"  + ch.enabled   + "</enabled>");
+  stream.println("<state>"    + ch.state     + "</state>");
+  stream.println("<time>"     + ch.startTime + "</time>");
+  stream.println("<duration>" + ch.duration  + "</duration>");
+  stream.println("<repeat>"   + ch.repeat    + "</repeat>");
+  stream.println("</channel>");
   stream.println("</channels>");
   stream.println('\n');
 }
@@ -84,10 +97,42 @@ void loop() {
   String request;
   if (readRequest(Serial3, request)) {
     Serial.println("[ESP8266]  " + request);
-    if ( request.indexOf("channel.xml") >= 0) {
-      Serial.print("[MEGA2560] Deliver channel.xml...");
-      parseToXml(Serial3, channels, 3);
-      Serial.println("Done");
+    int index = request.indexOf("channel.xml");
+    if (index >= 0) {
+      index = request.indexOf('?', index+11);
+      if (index >= 0) {
+        // Read channel number
+        int channelIdxStart = request.indexOf("channel=", index+1) + 8;
+        byte channelIdx = request.substring(channelIdxStart).toInt();
+
+        int startIdx = index;
+        int sepIdx, endIdx;
+
+        do {
+          sepIdx = request.indexOf('=', startIdx+1);
+          endIdx = request.indexOf('&', sepIdx+1);
+          if (endIdx == 0) {
+            endIdx = request.length();
+          }
+
+          String key   = request.substring(startIdx+1, sepIdx);
+          String value = request.substring(sepIdx+1, endIdx);
+          Serial.print(key);
+          Serial.print(" = ");
+          Serial.println(value);
+
+          // ToDo: Query content and parse to data object
+
+          startIdx = endIdx;
+        } while (endIdx < request.length());
+
+        parseToXml(Serial3, channels[channelIdx], channelIdx);
+      }
+      else {
+        Serial.print("[MEGA2560] Deliver channel.xml...");
+        parseToXml(Serial3, channels, 4);
+        Serial.println("Done");
+      }
     }
   }
 }
--- Arduino/ESP8266-WebServer/ESP8266-WebServer.ino
+++ Arduino/ESP8266-WebServer/ESP8266-WebServer.ino
@@ -1,6 +1,8 @@
 // Load Wi-Fi library
 #include <ESP8266WiFi.h>
-#include <FS.h>
+#include <ESPAsyncWebServer.h>
+
+#define TIMEOUT 1000
 
 // Replace with your network credentials
 //const char* ssid     = "gigacube-5193";
@@ -10,10 +12,7 @@ const char* password = "45299115630418744911";
 
 
 // Set web server port number to 80
-WiFiServer server(80);
-
-// Variable to store the HTTP request
-String request, response;
+AsyncWebServer server(80);
 
 void setup() {
   // Initialize serial
@@ -21,11 +20,11 @@ void setup() {
   Serial.println();
 
   // Initialize SPIFFS
-  if(!SPIFFS.begin()){
+  if (!SPIFFS.begin()){
     Serial.println("An Error has occurred while mounting SPIFFS");
     return;
   }
-  
+
   // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.print(ssid);
@@ -40,7 +39,72 @@ void setup() {
   Serial.println("WiFi connected.");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
-  
+
+  // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", "text/html");
+  });
+
+  // Route for xml requests
+  server.on("/channel.xml", HTTP_GET, [](AsyncWebServerRequest *request){
+    String head = request->url();
+    int params = request->params();
+
+    if (params > 0) {
+       head += '?';
+    }
+    
+    for (int i = 0; i < params; i++){
+      AsyncWebParameter* p = request->getParam(i);
+      head += p->name();
+      head += '=';
+      head += p->value();
+
+      if ((params - i) > 1) {
+        head += '&';
+      }
+    }
+
+    Serial.println(head);
+    Serial.flush();
+
+    unsigned long startTime = millis();
+    unsigned long duration = 0;
+    String response = "";
+    String currentLine = "";
+    do {
+      if (Serial.available()) {
+        char c = Serial.read();
+
+        // Check for newline
+        if (c == '\n') {
+          // If current line is empty, response is ready
+          if (currentLine.length() == 0 && response.length() > 0) {
+              break;
+          }
+          else if (currentLine.length() > 0) {
+            response += currentLine;
+            response += '\n';
+            currentLine = "";
+          }
+        }
+        else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+      duration = millis() - startTime;
+    } while (duration < TIMEOUT);
+
+    if (duration < TIMEOUT) {
+      request->send(200, "text/xml", response); 
+    }
+    else {
+      Serial.println("Timout while assembling response");
+      request->send(504, "text/plain", "Timout while assembling response"); 
+    }
+    
+  });
+
   // Start web server
   server.begin();
   Serial.println("Server started\n");
@@ -48,110 +112,5 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client) {                             // If a new client connects,
-    Serial.println("Client connected");
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        request += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            if (request.indexOf("channel") >= 0) {
-              // Forward request to arduino mega, but only the first line
-              Serial.println(request.substring(0, request.indexOf('\n')));
-              Serial.flush();
-
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/xml");
-              client.println("Connection: keep-alive");
-              client.println();
-    
-              // Wait for response from arduino mega
-              char s, n;
-              do {
-                if (Serial.available() >= 2) {
-                  s = Serial.read();
-                  n = Serial.peek();
-                  client.print(s);
-                }
-                yield();
-              } while (!(s == '\n' && n == '\n'));
-              client.println();
-              client.flush();
-            }
-            else {
-              Serial.print("Deliver index.html...");
-              sendFile(client, "/index.html");
-              Serial.println("Done");
-            }
-
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      } // end if (client.available())
-    } // end while (client.connected())
-    
-    // Clear the header variable
-    request = "";
-    
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected");
-  } // end if (client)
-}
-
-void sendFile(WiFiClient& cl, String filename) {
-  static File file;
-  file = SPIFFS.open(filename, "r");
-  
-  if (file) {
-    // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-    // and a content-type so the client knows what's coming, then a blank line:
-    cl.println("HTTP/1.1 200 OK");
-    cl.println("Content-type:text/" + filename.substring(filename.lastIndexOf('.')+1));
-    cl.println("Connection: keep-alive");
-    cl.println();
-  
-    String buffer = "";
-    while (file.available()) {
-      char c = file.read();
-      if (c == '\n') {
-        cl.println(buffer);
-        buffer = "";
-      }
-      else {
-        buffer += c;
-      }
-      yield();
-    }
-    file.close();
-  }
-  else {
-      cl.println("HTTP/1.1 400 Bad Request");
-      cl.println("Content-type:text/plain");
-      cl.println("Connection: close");
-      cl.println();
-      cl.println("Could not find file " + filename);
-  }
-  cl.flush();
-}
-
-// Assemble xml file
-void sendXMLFile(WiFiClient cl){
-  // Prepare XML file
-  cl.print("<?xml version = \"1.0\" ?>");
-  cl.print("<inputs>");
-
-  
-  cl.print("</inputs>");
 }
